412836b53255a026c8786f7f1d01dc40c5500c9c
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2024-05-02 20:35:44 -0500

    Set base URL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   ssr: false,
   app: {
+    baseURL: "/clean-cuts/",
     pageTransition: {
       name: "page",
       mode: "out-in",
