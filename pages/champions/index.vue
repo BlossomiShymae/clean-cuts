06e682968c757e384dc951abc17542f3c0f11acc
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2024-10-09 12:49:19 -0500

    Center champion cells

--- pages/champions/index.vue
+++ pages/champions/index.vue
@@ -15,26 +15,24 @@ watch(currentLocale, async () => {
 </script>
 
 <template>
-  <div class="d-flex flex-column gap-2">
-      <div class="d-flex flex-row flex-wrap gap-4">
-        <div v-for="summary in summaries" :id="`${summary.id}`" 
-            style="width: 200px;"
-            data-aos="zoom-out"
-            data-aos-duration="500">
-            <NuxtLink :to="`/champions/${summary.id}`">
-                <div class="ratio ratio-16x9 position-relative">
-                    <img class="object-fit-cover rounded" 
-                        :src="skins.find(x => (x.id / 1000) == summary.id)?.getTile({locale: currentLocale, version: 'latest'})"
-                        loading="lazy"/>
-                    <div class="position-absolute z-1 d-flex flex-column justify-content-end">
-                        <div class="d-inline-flex justify-content-between align-items-center px-2" style="background: #0008;">
-                            <span class="fs-5 fw-light">{{ summary.name }}</span>
-                            <span class="fw-bold">{{ summary.id }}</span>
-                        </div>
+  <div class="d-flex flex-row flex-wrap gap-4 justify-content-center">
+    <div v-for="summary in summaries" :id="`${summary.id}`" 
+        style="width: 200px;"
+        data-aos="zoom-out"
+        data-aos-duration="500">
+        <NuxtLink :to="`/champions/${summary.id}`">
+            <div class="ratio ratio-16x9 position-relative">
+                <img class="object-fit-cover rounded" 
+                    :src="skins.find(x => (x.id / 1000) == summary.id)?.getTile({locale: currentLocale, version: 'latest'})"
+                    loading="lazy"/>
+                <div class="position-absolute z-1 d-flex flex-column justify-content-end">
+                    <div class="d-inline-flex justify-content-between align-items-center px-2" style="background: #0008;">
+                        <span class="fs-5 fw-light">{{ summary.name }}</span>
+                        <span class="fw-bold">{{ summary.id }}</span>
                     </div>
                 </div>
-            </NuxtLink>
-        </div>
-      </div>
-  </div>
+            </div>
+        </NuxtLink>
+    </div>
+    </div>
 </template>
