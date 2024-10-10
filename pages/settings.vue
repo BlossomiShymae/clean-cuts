06f8e33000ca038dd786e26511c4c0c3322498d8
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2024-10-10 10:21:02 -0500

    Update settings page

--- pages/settings.vue
+++ pages/settings.vue
@@ -1,20 +1,17 @@
+<script setup lang="ts">
+const { locales, currentLocale, setLocale } = useLocale();
+</script>
+
 <template>
-  <div>
-    <div class="row">
-      <h1 class="display-4 mb-4">Settings</h1>
-      <div class="d-flex flex-column justify-items-stretch gap-1">
-        <div v-for="(value, key, index) in locales">
-          <button class="btn border border-1 w-100" :class="{'btn-secondary': currentLocale == key}" @click="setLocale(key)" >
-            {{ value }}
-          </button>
-        </div>
+  <div class="app-background p-4 rounded">
+    <div class="d-flex flex-column justify-items-stretch gap-1">
+      <div v-for="(value, key, index) in locales"
+        data-aos="fade-right"
+        data-aos-duration="500">
+        <button class="btn border border-1 w-100" :class="{'btn-secondary': currentLocale == key}" @click="setLocale(key)" >
+          {{ value }}
+        </button>
       </div>
     </div>
   </div>
 </template>
-
-<script setup lang="ts">
-import useLocale from "../composables/useLocale";
-
-const { locales, currentLocale, setLocale } = useLocale();
-</script>
