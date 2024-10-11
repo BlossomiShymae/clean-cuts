1b75c0b66e4704b81247183942f3401b3b2d26a5
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2024-10-10 19:29:09 -0500

    Update TFT damage skins

--- pages/tft-damage-skins/index.vue
+++ pages/tft-damage-skins/index.vue
@@ -1,43 +1,4 @@
-<template>
-  <div class="d-flex flex-column gap-2">
-    <h1 class="mb-3">TFT Damage Skins</h1>
-
-    <div class="input-group">
-      <input type="text" class="form-control bg-transparent border-light border-opacity-25" placeholder="Search" name="Search"
-        v-model="query"/>
-    </div>
-
-    <Pagination :pages="p.pages" :count="p.count" :index="p.index.value" :on-prev="p.prev" :on-next="p.next"
-    :on-first="p.first" :on-last="p.last"/>
-
-    <div class="d-flex flex-wrap justify-content-around gap-2">
-      <div class="d-inline-flex align-items-stretch justify-content-stretch" v-for="tftDamageSkin in p.pages[p.index.value]" :key="tftDamageSkin.itemId">
-        <NuxtLink class="text-decoration-none" :to="`/tft-damage-skins/overview/${tftDamageSkin.itemId}`">
-          <div class="card h-100 bg-tranparent bg-screen border-light border-opacity-25" style="max-width: 140px; width: 140px;">
-            <img :src="tftDamageSkin.getLoadoutsIcon('latest')" loading="lazy" onerror="this.onerror = null; this.src='/clean-cuts/img/error.png';" />
-
-            <div class="card-body d-flex flex-column justify-content-end">
-              <h5 class="card-title">{{ tftDamageSkin.name }}</h5>
-              <Badge name="identifier">{{ tftDamageSkin.itemId }}</Badge>
-            </div>
-          </div>
-        </NuxtLink>
-      </div>
-    </div>
-
-    <Pagination :pages="p.pages" :count="p.count" :index="p.index.value" :on-prev="p.prev" :on-next="p.next"
-    :on-first="p.first" :on-last="p.last"/>
-  </div>
-</template>
-
 <script setup lang="ts">
-import Pagination from '~/components/Pagination.vue';
-import Badge from '~/components/Badge.vue';
-import useClient from '~/composables/useClient';
-import useLocale from '~/composables/useLocale';
-import useIsNumeric from '~/composables/useIsNumeric';
-import usePagination from '~/composables/usePagination';
-
 const { client } = useClient();
 const { currentLocale } = useLocale();
 const getTftDamageSkins = async () => (await client.tftDamageSkins.listAsync({ locale: currentLocale.value, version: "latest"}))
@@ -60,3 +21,36 @@ const p = computed(() => {
   return usePagination(filtered, 24);
 })
 </script>
+
+<template>
+  <div class="d-flex flex-column gap-4">
+    <div class="d-flex justify-content-end gap-2">
+      <Card class="d-flex justify-content-center align-items-center me-auto">
+        <span>{{ tftDamageSkins.length }} TFT damage skins</span>
+      </Card>
+      <div class="input-group" style="max-width: 400px;">
+        <input type="text" class="form-control bg-transparent border-light border-opacity-25" placeholder="Search" name="Search"
+          v-model="query"/>
+      </div>
+    </div>
+
+    <div class="d-flex flex-wrap gap-4 justify-content-center">
+      <div style="width: 200px;" v-for="tftDamageSkin in tftDamageSkins" :id="`${tftDamageSkin.itemId}`"
+        data-aos="zoom-out"
+        data-aos-duration="500">
+        <div class="ratio ratio-1x1 position-relative">
+          <img class="rounded" :src="tftDamageSkin.getLoadoutsIcon('latest')" loading="lazy"/>
+          <div class="position-absolute z-1 d-flex flex-column justify-content-end">
+            <div class="d-inline-flex justify-content-between align-items-end bg-dark-gradient p-2">
+              <span>{{ tftDamageSkin.name }}</span>
+              <span>{{ tftDamageSkin.itemId }}</span>
+            </div>
+          </div>
+          <div class="position-absolute z-2 d-flex flex-column justify-content-start align-items-end">
+            <span class="rounded px-1 m-1" style="background: #0008; padding: 1px;">{{ tftDamageSkin.rarity }}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
